4e91470beda279f819c1a9236a7de3fe9d12b4b5
Qeti Avanashvili <qetiavanashvili@gmail.com>
2023-04-03 13:12:37 +0400

    Update function

--- GeorgianMemenator/Scenes/DogBreedDetail/DogBreedDetailViewModel.swift
+++ GeorgianMemenator/Scenes/DogBreedDetail/DogBreedDetailViewModel.swift
@@ -4,15 +4,17 @@
 //
 //  Created by Qeti Avanashvili on 22.03.23.
 //
-import SwiftUI
 import Foundation
 import Combine
 
 class DogBreedDetailViewModel: ObservableObject {
     @Published var dogBreedDetail: DogBreed?
+    @Published private(set) var hasError: Bool = false
+    @Published private(set) var isLoading: Bool = false
     
     private let dogBreedDetailService: DogBreedsServing
     private let dogBreedId: String
+    private var cancellables = Set<AnyCancellable>()
     
     init(dogBreedDetailService: DogBreedsServing, dogBreedId: String) {
         self.dogBreedDetailService = dogBreedDetailService
@@ -22,5 +24,17 @@ class DogBreedDetailViewModel: ObservableObject {
     func getDogBreedDetail() {
         dogBreedDetailService
             .getDogBreedDetails(with: dogBreedId)
+            .sink(
+                receiveCompletion: { [weak self] val in
+                    if case .failure = val {
+                        self?.isLoading = false
+                        self?.hasError = true
+                    }
+                }, receiveValue: { [weak self] val in
+                    self?.dogBreedDetail = val
+                    self?.isLoading = false
+                    self?.hasError = false
+                })
+            .store(in: &cancellables)
     }
 }
