807c15009ca848333abfe8f729dca1bd5d3d00d2
Qeti Avanashvili <qetiavanashvili@gmail.com>
2023-04-03 12:24:55 +0400

    Add service function in dogBreedDetaiViewmodel

--- GeorgianMemenator/Scenes/DogBreedDetail/DogBreedDetailViewModel.swift
+++ GeorgianMemenator/Scenes/DogBreedDetail/DogBreedDetailViewModel.swift
@@ -4,9 +4,23 @@
 //
 //  Created by Qeti Avanashvili on 22.03.23.
 //
-
+import SwiftUI
 import Foundation
+import Combine
 
 class DogBreedDetailViewModel: ObservableObject {
+    @Published var dogBreedDetail: DogBreed?
     
+    private let dogBreedDetailService: DogBreedsServing
+    private let dogBreedId: String
+    
+    init(dogBreedDetailService: DogBreedsServing, dogBreedId: String) {
+        self.dogBreedDetailService = dogBreedDetailService
+        self.dogBreedId = dogBreedId
+    }
+   
+    func getDogBreedDetail() {
+        dogBreedDetailService
+            .getDogBreedDetails(with: dogBreedId)
+    }
 }
